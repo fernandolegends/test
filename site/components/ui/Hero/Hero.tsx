c3633a97cac87854a7873370fb7e0d5ebe26015d
fernando141 <fernando@snaptravel.com>
2022-04-18 16:37:02 -0400

    Swiper JS integration

--- site/components/ui/Hero/Hero.tsx
+++ site/components/ui/Hero/Hero.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react'
 import { Container } from '@components/ui'
-import { ArrowRight } from '@components/icons'
 import s from './Hero.module.css'
 import Link from 'next/link'
 interface HeroProps {
@@ -19,8 +18,7 @@ const Hero: FC<HeroProps> = ({ headline, description }) => {
             <p>{description}</p>
             <Link href="/">
               <a className="flex items-center text-accent-0 pt-3 font-bold hover:underline cursor-pointer w-max-content">
-                Read it here
-                <ArrowRight width="20" heigh="20" className="ml-1" />
+                Shop Now
               </a>
             </Link>
           </div>
